0a2737175dad33ba35fe828d38f18531a7f5d939
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2023-11-04 22:27:30 -0300

    Testes automatizados

--- testes/01/service.js
+++ testes/01/service.js
@@ -3,14 +3,18 @@ const axios = require('axios');
 const URL = `https://swapi.dev/api/people`;
 
 async function obterPessoas(nome){
-    
     const url = `${URL}/?search=${nome}&format=json`;
-    
-    const response = await axios.get(url);
-
-    return response.data;
-
+    const result = await axios.get(url);
+    return result.data.results.map(mapearPessoas);
 }
+
+function mapearPessoas(item){
+    return {
+        nome: item.name,
+        peso: item.height
+    }
+}
+
 module.exports = {
     obterPessoas
 }
